cac98072315844a5d77513ab51a77e07fee59c5c
Meena Alfons <meena.kerolos@gmail.com>
2026-06-07 11:52:50 +0200

    hide share link

--- assets/AddSessionDialog-CtMYQE9Z.js
+++
@@ -1,1 +0,0 @@
-import{o as r}from"./chunk-QMGIS6GS-B5Ud1i36.js";import{F as m}from"./index-C3231fep.js";import{S as d}from"./SessionDialog-BvNOe-ze.js";function f({open:o,setOpen:s,predefinedProgram:a}){const[i,{}]=m();return r.jsx(d,{open:o,setOpen:s,predefinedProgram:a,onSave:async({name:n,programId:t,happensAt:e})=>{await i({name:n,programId:t,happensAt:e}).unwrap()}})}export{f as A};
---
+++ assets/AddSessionDialog-DYv6wILN.js
@@ -0,0 +1,1 @@
+import{o as r}from"./chunk-QMGIS6GS-B5Ud1i36.js";import{F as m}from"./index-CGmuhmqX.js";import{S as d}from"./SessionDialog-DJEBflFl.js";function f({open:o,setOpen:s,predefinedProgram:a}){const[i,{}]=m();return r.jsx(d,{open:o,setOpen:s,predefinedProgram:a,onSave:async({name:n,programId:t,happensAt:e})=>{await i({name:n,programId:t,happensAt:e}).unwrap()}})}export{f as A};
